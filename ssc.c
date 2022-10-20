75163f049638c0ce28ee1a5bdf4db583ae8edffc
jvmf1 <jvx8ss@gmail.com>
2022-10-19 22:38:27 -0300

    improved speed

--- ssc.c
+++ ssc.c
@@ -4,44 +4,26 @@
 
 #define SHUFFLES 4
 
-static int sum(unsigned char *data, size_t len) {
-	int r = 0;
-	for (size_t i = 0; i < len; i++)
-		r+=data[i];
-	return r;
-}
-
 static void xor(const unsigned char *a, const unsigned char *b, const size_t len, unsigned char *out) {
 	for (size_t i = 0; i < len; i++)
 		out[i] = a[i] ^ b[i];
 }
 
-static void shuffle(const unsigned char *a, const size_t len, int random, unsigned char *out) {
+static void shuffle(unsigned char *a, const size_t len, int nonce) {
 	for (size_t i = 0; i < len; i++) {
-		out[i] = (a[i] << 3) + (i*137/len) + (a[(i*87+3)%len]*27 ^ random);
+		a[i] = (a[i] << 3) + (i*137/len) + (a[(i*87+3)%len]*27 ^ nonce);
 	}
 }
 
 void ssc_cipher(const unsigned char *data, const size_t len, const unsigned char *key, const size_t key_len, const int nonce, unsigned char *out) {
 
-	unsigned char keys[2][len];
+	unsigned char k[len];
 
-	unsigned char *k1 = &keys[0][0];
-	unsigned char *k2 = &keys[1][0];
+	for (int i = 0; i < len; i++)
+		k[i] = key[i % key_len];
 
-	unsigned char tmp[len];
+	for (int i = 0; i < SHUFFLES; i++)
+		shuffle(k, len, nonce);
 
-	memcpy(tmp, data, len);
-
-	for (int i = 0; i < len; i++) {
-		keys[0][i] = key[i % key_len];
-		keys[1][i] = key[i % key_len];
-	}
-
-	for (int i = 0; i < SHUFFLES; i++) {
-		shuffle(k1, len, sum(k1, len)^nonce, k2);
-		k1 = k2;
-	}
-
-	xor(data, k2, len, out);
+	xor(data, k, len, out);
 }
