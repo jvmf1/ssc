b19102b327b0b17500c2585b079e88ed4b761337
jvmf1 <jvx8ss@gmail.com>
2022-10-20 16:37:00 -0300

    added --rgb, padding in visualize.py

--- visualize.py
+++ visualize.py
@@ -4,12 +4,39 @@ import numpy as np
 import cv2
 import sys
 import math
+import argparse
 
-arr = bytearray(sys.stdin.buffer.read())
+parser = argparse.ArgumentParser()
+parser.add_argument("--rgb", action='store_true')
+args = parser.parse_args()
 
-size = int(math.sqrt(len(arr)))
+orig_arr = bytearray(sys.stdin.buffer.read())
 
-arr = np.array(arr).reshape((size,size)).astype(np.uint8)
+
+if args.rgb:
+
+    size = math.ceil(math.sqrt(len(orig_arr)/3))
+
+    dim = (size,size,3)
+
+else:
+
+    size = math.ceil(math.sqrt(len(orig_arr)))
+
+    dim = (size, size)
+
+if np.prod(dim) > len(orig_arr):
+
+    arr = np.append(orig_arr, np.zeros(np.prod(dim) - len(orig_arr)))
+
+else:
+
+    arr = orig_arr
+
+arr = np.array(arr).reshape(dim).astype(np.uint8)
+
+if np.prod(arr.shape) != len(orig_arr):
+    print(f"Padding {len(orig_arr)-len(arr)} bytes")
 
 cv2.namedWindow("a", cv2.WINDOW_KEEPRATIO)
 cv2.imshow("a", arr)
