a568b894c404dfada996a857936dc174958ad41e
jvmf1 <jvx8ss@gmail.com>
2022-10-20 00:07:25 -0300

    improved shuffle randomness

--- ssc.c
+++ ssc.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 #include "ssc.h"
 
 #define SHUFFLES 4
@@ -10,9 +11,8 @@ static void xor(const unsigned char *a, const unsigned char *b, const size_t len
 }
 
 static void shuffle(unsigned char *a, const size_t len, int nonce) {
-	for (size_t i = 0; i < len; i++) {
-		a[i] += (i^nonce) ^ (a[(i*87)%len]);
-	}
+	for (size_t i = 0; i < len; i++)
+		a[i] += ((i*137)/len) ^ (a[(i*87)%len]^nonce);
 }
 
 void ssc_cipher(const unsigned char *data, const size_t len, const unsigned char *key, const size_t key_len, const int nonce, unsigned char *out) {
