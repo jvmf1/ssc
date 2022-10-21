1c0b709a6337afdf55eb86d49fa1fdb5e3d3beed
jvmf1 <jvx8ss@gmail.com>
2022-10-21 09:27:39 -0300

    added key_len in shuffle

--- ssc.c
+++ ssc.c
@@ -10,9 +10,9 @@ static void xor(const unsigned char *a, const unsigned char *b, const size_t len
 		out[i] = a[i] ^ b[i];
 }
 
-static void shuffle(unsigned char *a, const size_t len, int nonce) {
+static void shuffle(unsigned char *a, const size_t len, int nonce, size_t key_len) {
 	for (size_t i = 0; i < len; i++) {
-		unsigned char tmp = a[(i*87)%len];
+		unsigned char tmp = a[((i*87)+key_len)%len];
 		a[i] += 1 + ((tmp >> 5) + a[tmp%len]) + ((nonce + tmp) >> 3) * i;
 	}
 }
@@ -25,7 +25,7 @@ void ssc_cipher(const unsigned char *data, const size_t len, const unsigned char
 		k[i] = key[i % key_len];
 
 	for (int i = 0; i < SHUFFLES; i++)
-		shuffle(k, len, nonce);
+		shuffle(k, len, nonce, key_len);
 
 	xor(data, k, len, out);
 }
